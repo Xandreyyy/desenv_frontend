4109b9334b027c8c3fc7fd6db4d2065a61612708
Alexandre <alejandroesteves2005@gmail.com>
2023-05-18 16:57:23 -0300

    Update

--- Exercices/AGENDA DE CONTATOS/main.js
+++ Exercices/AGENDA DE CONTATOS/main.js
@@ -3,11 +3,26 @@ const menuPadrao = document.getElementById("opcoes_padrao")
 const menuAdd = document.querySelector(".addContato")
 const formAdd = document.getElementById("form_add")
 
-const semEspaco = document.querySelector("#num_nospace")
+//possíveis erros
 const semPais = document.querySelector("#num_sempais")
+const numInv = document.querySelector("#num_invalido")
+
+function erroGenerico() {
+    numInv.style.display = "block"
+    numInv.innerHTML = "Número inválido."
+
+    semPais.style.display = "none"
+    numInv.style.display = "none"
+}
+
+function erroPais() {
+    semPais.style.display = "block"
+    semPais.innerHTML = "Selecione um país!"
+
+    numInv.style.display = "none"
+}
 
 let nomeContato, numeroCelular
-
 //abre menu para add
 botaoAdd.addEventListener("click", function(e){
     e.preventDefault()
@@ -43,8 +58,7 @@ const select_paises = document.getElementById("select_paises")
 select_paises.addEventListener("change", function(e){
     let ddd = e.target.value
     if (select_paises.value == ""){
-        semPais.style.display = "block"
-        semPais.innerHTML = "Selecione um país!"
+        erroPais()
         numeroCelular_obj.value = null
     }else{
         numeroCelular_obj.value = `${ddd} `
@@ -63,10 +77,22 @@ function validarPais() {
     }
 }
 
+function validarTam(numeroCel) {
+    const arrayNum = numeroCel.split(" ")
+    if (arrayNum.length ==3){
+        numInv.style.display = "none"
+        return true
+    }else{
+        numInv.style.display = "block"
+        numInv.innerHTML = "Número inválido."
+        return false
+    }
+}
+
 //Uma opção para validar a entrada é adicionar: numeroCelular.startsWith("+"). "startsWith()" verifica se uma string inicia com tal caracter.
 formAdd.addEventListener("submit", function(e){
     e.preventDefault()
-    if (validarPais()){
+    if (validarPais() && validarTam(numeroCelular_obj.value)){
         tabela_obj = document.querySelector("#tBody")
         linha_tabela = document.createElement("tr")
         tabela_obj.appendChild(linha_tabela)
@@ -88,3 +114,10 @@ formAdd.addEventListener("submit", function(e){
         menuPadrao.style.display = "block"
     }
 })
+
+// Como usar jQuery
+// 1- Baixar a jQuery na pasta do projeto (contém muitos arquivos, pode acontecer que o projeto tenha uma resposta mais lenta)
+// 2- Importar a jQuery, ou seja, usar remotamente.
+// 
+//
+//
