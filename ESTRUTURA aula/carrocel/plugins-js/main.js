ca0b3cd904bcd6184b198732873288e985c52d61
Alexandre <alejandroesteves2005@gmail.com>
2023-05-24 17:29:56 -0300

    Update

---
+++ ESTRUTURA aula/carrocel/plugins-js/main.js
@@ -0,0 +1,60 @@
+//plugin do igorescobar: igorescobar/jQuery-Mask-Plugin/archive/master.zip
+//jquery validation: https://jqueryvalidation.org/
+$(document).ready(function(){
+    $("#carrossel_imagens").slick({
+        autoplay: true,
+        arrows: false,
+        autoplaySpeed: 1000,
+    })
+
+    $(".menu_hamburguer").click(function(){
+        $("nav").slideToggle()
+    })
+
+    $("#telefone").mask('(00) 00000-0000', {
+        placeholder: '(12) 34567-8910'
+    })
+
+    $("form").validate({
+        rules:{
+            nome:{
+                required: true
+            },
+            telefone:{
+                required: true
+            },
+            email:{
+                required: true,
+                email: true,
+            },
+            veiculo_interesse:{
+                required: true
+            },
+        },
+        messages:{
+            nome: "Por favor, informe seu nome!",
+            telefone: "Por favor, informe seu telefone!",
+            email: "Por favor, informe seu e-mail!",
+            veiculo_interesse: "Por favor, informe o veículo desejado!",
+        },
+        submitHandler: function(form){
+            console.log(form)
+        },
+        invalidHandler: function(e, validador){
+            let camposIncorretos = validador.numbersOfInvalids()
+            if (camposIncorretos){
+                alert(`Existem ${camposIncorretos} campos incorretos!`)
+            }
+        }
+    })
+    $(".lista_veiculos button").click(function(){
+        const destino = $("#formulario")
+        const nomeVeiculo = $(this).parent().find('h3').text()
+
+        $("#veiculo_interesse").val(nomeVeiculo)
+
+        $("html").animate({
+            scrollTop: destino.offset().top
+        }, 1000)
+    })
+})
