44d30cec255c242538fae43d7584d34be427555a
Alexandre <alejandroesteves2005@gmail.com>
2023-06-06 17:29:34 -0300

    Update

--- Exercices/calcular_imc/js/main.js
+++ Exercices/calcular_imc/js/main.js
@@ -17,7 +17,7 @@ const regexCm = /^0/
 
 peso.addEventListener("keyup", function(){
     let i = this
-    //regex: ao botar \D junto com a negação, o \D se torna \d, por isso usei ^\d, o resto dá para entender
+    //regex: ao botar \D junto com a negação (^), o \D se torna \d, por isso usei ^\d. Ele faz com que tudo que não é dígito, "." ou ",", substitui por ""
     i.value = i.value.replace(/[^\d\.,]/g, "")
 
     if (regexCm.test(i.value)){
@@ -30,6 +30,7 @@ peso.addEventListener("keyup", function(){
         msgPeso.innerHTML = null
     }
 
+    //verifica se o valor que acabou de ser digitado é != "", então aplica um splice na posição 0, 1 elemento para modificar e passo o elemento que vai modificar o array
     if (i.value != ""){
         arrayPeso.splice(0, 1, i.value)
         console.log(`Valor peso sendo pushado: ${i.value}`)
@@ -58,10 +59,13 @@ altura.addEventListener("keyup", function(){
     inputsResets()
 })
 
-
+//esta função serve para "resetar" a página, ela é chamada toda vez que uma tecla do teclado é pressionada
 function inputsResets() {
+    //esse teste é para garantir que o reset funcione só quando algo válido for digitado, ou seja, se letras forem digitadas, o reset não se aplica,
+    //do contrário os respectivos array estarão com conteúdo dentro, e o teste lógico dará true.
+    //removerResul1 pega o span com o atual id que é dado junto com o titleInject(), que retorna um número de acordo com o resultado do IMC (esse span é a setinha que indica na tabela o resultado)
+    //depois injeto um valor vazio no span do resultado
     if (arrayAltura.length !== 0 && arrayPeso.length !== 0){
-        //bloco para resetar página
         const removerResul1 = document.getElementById(`span${titleInject()}`)
         removerResul1.innerHTML = null
 
@@ -143,21 +147,25 @@ function inputsResets() {
 // }
 
 function verificarPeso(pValue) {
-    const vPeso = pValue.value;
-    const regexPeso = /^[\d]{1,3}([\.,]{1}[\d]{1,2})?$/;
+    const vPeso = pValue.value
+    //regex: validar se a entrada tem de 1 ou até 3 dígitos, e caso o usuário coloque "." ou ",", ele é obrigado a colocar pelo menos 1 dígito e no máximo 2
+    const regexPeso = /^[\d]{1,3}([\.,]{1}[\d]{1,2})?$/
 
     if (!regexPeso.test(vPeso)) {
-      msgPeso.style.display = "block";
-      msgPeso.innerHTML = "Peso inválido!";
-      return false;
+      msgPeso.style.display = "block"
+      msgPeso.innerHTML = "Peso inválido!"
+      return false
     } else {
-      msgPeso.style.display = "none";
-      return true;
+      msgPeso.style.display = "none"
+      return true
     }
 }
 
+//essas duas funções (verificarPeso e verificarAltura) são chamadas no botão de submit
+
 function verificarAltura(aValue) {
     const vAltura = aValue.value
+    //regex: só é possível colocar 1 dígito, já que na realidade não existe um ser humano que possui 2 dígitos em m de altura kkkkkkkkkk. Mesma coisa que o regex anterior, caso ele coloque "." ou "," é obrigado a color 1 ou 2 dígitos
     const regexAltura = /^[\d]{1}([\.,]{1}[\d]{1,2})?$/g
     if (!regexAltura.test(vAltura)){
         msgAltura.style.display = "block"
@@ -168,6 +176,7 @@ function verificarAltura(aValue) {
     }
 }
 
+//alterei 
 let imc = 0
 function calcIMC(pesoV, alturaV) {
     imc = parseFloat(pesoV.value) / parseFloat((alturaV.value * alturaV.value))
