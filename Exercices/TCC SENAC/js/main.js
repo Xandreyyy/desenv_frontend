861473abdaec592bd7d75b56eec9be6989126ab7
Alexandre <alejandroesteves2005@gmail.com>
2023-06-07 17:29:23 -0300

    Update

--- Exercices/TCC SENAC/js/main.js
+++ Exercices/TCC SENAC/js/main.js
@@ -1,6 +1,7 @@
 $(document).ready(function(){
     // deixar botões de novidades dinâmicos
     $("#mostrarMais_novidades").on("click", function(){
-        $("#novidadesplus").slideDown("fast", function(){})
+        $("#novidadesplus").slideDown()
+        $("#novidadeplus").addClass("novidade")
     })
 })
