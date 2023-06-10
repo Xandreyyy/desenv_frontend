fc706af8189ad2d46c38a66787834f43bdd1d56b
Xandreyyy <alejandroesteves2005@gmail.com>
2023-06-10 04:21:07 -0300

    Update

--- Exercices/TCC SENAC/js/main.js
+++ Exercices/TCC SENAC/js/main.js
@@ -1,10 +1,45 @@
 $(document).ready(function(){
     // deixar botões de novidades dinâmicos
-    $("#mostrarMais_novidades").on("click", function(){
-        $("#novidadesplus").slideDown()
-        $("#novidadeplus").addClass("novidade")
+    const btnNovidades = $("#divMostrarNov")
+    const divNovidades = $("#novidadesplus")
+    $(btnNovidades).on("click", function(){
+        if (!btnNovidades.hasClass("mostrarMenos")){
+            divNovidades.slideDown()
+            divNovidades.css("display", "flex")
+
+            btnNovidades.appendTo(divNovidades).css("align-items", "flex-end")
+            btnNovidades.addClass("mostrarMenos")
+            btnNovidades.html('<p>mostrar menos<span class="span_setaNoticia">△</p></span>')
+        }else{
+            divNovidades.slideUp()
+            btnNovidades.removeClass("mostrarMenos")
+            btnNovidades.appendTo("#novidadesPadrao")
+            btnNovidades.html('<p>mostrar mais<span class="span_setaNoticia">▽</p></span>')
+        }
     })
 })
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 // .container_novidade {
 //     display: flex;
@@ -18,11 +53,3 @@ $(document).ready(function(){
 //   A propriedade flex-wrap: wrap garante que as divs filhas possam quebrar para a próxima linha quando necessário.
 //   A classe .novidade define a largura das divs individuais para ocupar 50% da largura do contêiner pai, permitindo que duas divs sejam exibidas lado a lado.
 //   Com esses estilos, as divs com a classe .novidade serão organizadas em duas colunas e várias linhas, dependendo do espaço disponível na largura da tela.
-
-  
-  
-  
-  
-  
-  
-  
